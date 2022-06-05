1aa0d53faaf7357ae43272628a24aa500d1a6dd6
Damian <damianabalos@hotmail.com>
2022-06-04 21:23:30 -0300

    adaptando a heroku 2

--- src/routers/autenticacion.js
+++ src/routers/autenticacion.js
@@ -287,7 +287,7 @@ rutaAutenticacion.get("/carrito", (req, res) => {
   const renderizar = () => {
     let cartProducts = productosCargadosAlCarrito[0]
     console.log(productosCargadosAlCarrito)
-    res.render("pages/carrito", { usuario, cartProducts })
+    res.render("pages/carrito", { usuario, cartProducts, productosCargadosAlCarrito })
   }
   setTimeout(() => {
     renderizar()
